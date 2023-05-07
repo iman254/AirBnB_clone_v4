4771b7c2fdaa76459a706f1da678d1d2f2d07c94
Benson Mwangi <bensonmwangi101@gmail.com>
2023-05-07 11:27:51 -0700

    Added all files

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -35,13 +35,13 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-                           cache_id=uuid.uuid4())
+                           places=places)
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
